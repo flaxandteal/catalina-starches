8c0dc0fc6cd72a932ade850cc09a08011f4fb422
Phil Weir <phil.weir@flaxandteal.co.uk>
2026-01-12 14:56:54 +0000

    fix: split out banner images

--- assets/index.ts
+++ assets/index.ts
@@ -1,17 +1,20 @@
-import { blob } from 'node:stream/consumers';
 import { initSwiper } from './swiper';
+import * as params from '@params';
 
-const bannerImages = [
-    { name: "600209 - Old Museum Building - 20090612 from E 010s.jpg", alt: "600209 - Old Museum Building" },
-    { name: "600242 - Home - 20240510_121923.jpg", alt: "600242 - Home" },
-    { name: "600432 - Raine Island Beacon - (EHP) - l.rai.90.205.11.7s.jpg", alt: "600432 - Raine Island Beacon" },
-    { name: "600432_Raine Island Beacon (2015)_085.jpg", alt: "600432 - Raine Island Beacon" },
-    { name: "600432_Raine Island Beacon (2015)_101.jpg", alt: "600432 - Raine Island Beacon" },
-    { name: "600458 - Carcory Homestead Ruins (2016) -020.jpg", alt: "600458 - Carcory Homestead Ruins" },
-    { name: "602825 Oribin Studio streetscape views (2012) - Explorer.jpg", alt: "602825 - Oribin Studio" },
-    { name: "645612 - Mount Elliott Mining Complex - 3.jpg", alt: "645612 - Mount Elliott Mining Complex" },
-    { name: "645612 - Mount Elliott Mining Complex - Smelter precinct.jpg", alt: "645612 - Mount Elliott Mining Complex" },
-    { name: "650004 - Maroon State School - Teaching Blg looking SE_2015_1_public_SQUARE.jpg", alt: "650004 - Maroon State School" }
-]
+interface BannerImage {
+    name: string;
+    alt: string;
+}
 
-initSwiper(bannerImages, 'img').then(() => document.querySelector('.hero-banner')?.classList.add('loaded'));
+interface BannerImagesConfig {
+    path: string;
+    images: BannerImage[];
+}
+
+const bannerConfig: BannerImagesConfig | undefined = params.banner_images;
+
+if (bannerConfig?.images) {
+    initSwiper(bannerConfig.images, bannerConfig.path).then(() =>
+        document.querySelector('.hero-banner')?.classList.add('loaded')
+    );
+}
