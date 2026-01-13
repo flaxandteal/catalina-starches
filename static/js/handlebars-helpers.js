6164abc7b10a62c1075cb682194635433d2945e1
Stuart Marshall <stuart.marshall@flaxandteal.co.uk>
2026-01-13 11:33:34 +0000

    fix: add clean helper to handlebars

--- static/js/handlebars-helpers.js
+++ static/js/handlebars-helpers.js
@@ -29,6 +29,12 @@
   Handlebars.registerHelper("in", (a, b) => Array.isArray(b) ? b.includes(a) : (a in b));
   Handlebars.registerHelper("nospace", (a) => a.replaceAll(" ", "%20"));
   Handlebars.registerHelper("escapeExpression", (a) => Handlebars.Utils.escapeExpression(a));
+  Handlebars.registerHelper("clean", (a) => {
+    if (a && typeof a === 'object' && '__clean' in a) {
+      return a.__clean;
+    }
+    return a;
+  });
   Handlebars.registerHelper("concat", (...args) => args.slice(0, args.length - 1).join(""));
   Handlebars.registerHelper("array", (...args) => args);
   Handlebars.registerHelper("dialogLink", (options) => {
