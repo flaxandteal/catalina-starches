fd896abe7ae769fc27cf0fc340be3cfdb14b0145
Phil Weir <phil.weir@flaxandteal.co.uk>
2026-04-16 12:45:50 +0100

    fix: single build JSON

---
+++ utils/unify.py
@@ -0,0 +1,12 @@
+import json
+import os
+
+PREFIX = 'prebuild/business_data'
+
+all_bd = []
+for f in os.listdir(PREFIX):
+  if f.startswith('t_'):
+    with open(PREFIX + '/' + f) as fd:
+      all_bd += json.load(fd)['business_data']['resources']
+with open(PREFIX + '/a_all.json', 'w') as f:
+    json.dump({'business_data': {'resources': all_bd}}, f)
