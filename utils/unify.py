ffa2abbabb817b58bd67d8f434c48db9f5cad4a1
Phil Weir <phil.weir@flaxandteal.co.uk>
2026-05-14 17:35:28 +0100

    fix: remove unify debug

--- utils/unify.py
+++ utils/unify.py
@@ -9,8 +9,6 @@ for f in os.listdir(PREFIX):
     with open(PREFIX + '/' + f) as fd:
       resources = json.load(fd)['business_data']['resources']
       all_bd += resources
-      for res in resources:
-        if '691be2cf-25d8-5fe5-8ba7-93fe86217beb' == res['resourceinstance']['resourceinstanceid']:
-          print(f)
+
 with open(PREFIX + '/a_all.json', 'w') as f:
     json.dump({'business_data': {'resources': all_bd}}, f, indent=2)
